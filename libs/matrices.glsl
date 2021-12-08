83b13f36285f2cf2082e1b6de6004834c4a349d4
Davide Prati <lastexxit@gmail.com>
2021-12-08 14:23:04 +0100

    add scale

--- libs/matrices.glsl
+++ libs/matrices.glsl
@@ -2,3 +2,8 @@ mat2 rotate2d(float _angle){
     return mat2(cos(_angle),-sin(_angle),
                 sin(_angle),cos(_angle));
 }
+
+mat2 scale(vec2 _scale){
+    return mat2(_scale.x,0.0,
+                0.0,_scale.y);
+}
